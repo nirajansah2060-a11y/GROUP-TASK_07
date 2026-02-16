ed537c25e75c83aa8e6e801d219626e05ef72459
nirajansah2060-a11y <nirajansah2060@gmail.com>
2026-02-16 18:00:57 +0545

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
-const express = require('express');
-const app = express();
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-    res.send('CampusCycle is running');
+const express = require("express");
+const path = require("path");
+
+const indexRoutes = require("./routes/index");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// View engine
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", indexRoutes);
+
+// Basic 404
+app.use((req, res) => {
+  res.status(404).send("404 - Not Found");
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+// Start server
+app.listen(PORT, () => {
+  console.log(`CampusCycle running at http://localhost:${PORT}`);
 });
